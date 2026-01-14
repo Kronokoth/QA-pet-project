21cf9710eecb3aec9de1c9b9fe427707609aebcf
Kronokoth <112824445+Kronokoth@users.noreply.github.com>
2026-01-14 15:04:07 +0700

    test

--- tests/test_user.py
+++ tests/test_user.py
@@ -7,10 +7,12 @@ class TestUsers:
 
     @pytest.mark.users
     @allure.title("Create new user")
+    # test
     def test_create_user(self):
         self.user_api = UserAPI()
         return_user = self.user_api.create_user()
         username = return_user[1]['username']
         self.user_api.get_user_by_username(username)
         self.user_api.delete_user(username)
+
 
